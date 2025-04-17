42ed39c1cda5b3549843582cb07e2060fde6e5c9
Sophia Galvão <159277699+sophiaglv@users.noreply.github.com>
2025-04-17 08:49:02 -0300

    Delete Pets directory

--- Pets/src/main/java/com/pets/repository/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pets.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pets.entities.Pet;
-
-public interface PetRepository extends JpaRepository<Pet, Long>{
-
-}
